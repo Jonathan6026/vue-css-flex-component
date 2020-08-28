9a1d41053e2db9bc0885d77a382d26bdc93f93a7
Jonathan6026 <66056467+Jonathan6026@users.noreply.github.com>
2020-08-28 16:47:44 +0800

    项目布置&photo样式

---
+++ vuecss/src/components/common/photo.vue
@@ -0,0 +1,37 @@
+<template>
+	<div>
+		<div class="content">
+			<img src="../../assets/001.jpg" alt="">
+			<p>周末去游泳吧</p>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name:'Photo'
+	}
+</script>
+
+<style scoped="scoped">
+	
+	.content {
+		width: 100%;
+		height: 8rem;
+		position: relative;
+	}
+	img {
+		width: 100%;
+		height: 8rem;
+		
+	}
+	p {
+		width: 100%;
+		font-size: 0.75rem;
+		color: #eee;
+		background-color: rgba(0,0,0,.3);
+		position: absolute;
+		bottom: 0.05rem;
+		left: 0.2rem;
+	}
+</style>
---
+++ vuecss/src/components/index.vue
@@ -0,0 +1,17 @@
+<template>
+	<photo></photo>
+</template>
+
+<script>
+	import Photo from './common/photo.vue' 
+	
+	export default {
+		name:'Index',
+		components:{
+			Photo
+		}
+	}
+</script>
+
+<style>
+</style>
